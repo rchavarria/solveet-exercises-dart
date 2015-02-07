a74ebcfcc70572f3652fadd537dfca767e4a062e
rchavarria <txingo@gmail.com>
2015-02-07 17:30:23 +0100

    Use pow() method

--- test/football-tshirts/football_tshirts.dart
+++ test/football-tshirts/football_tshirts.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:unittest/unittest.dart';
 
 void main() {
@@ -7,7 +8,7 @@ void main() {
         test('Iterative', () {
             for(int x = 1; x < 100; x++) {
                 for(int y = 1; y < 100; y++) {
-                    int z = (x + y) * (x + y);
+                    int z = pow(x + y, 2);
                     if (x.toString() + y.toString() == z.toString()) {
                         print('x: ${x}, y: ${y} => z: ${z}');
                     }
