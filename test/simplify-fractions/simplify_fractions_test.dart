85c941ececce2c3332859676f06c7cd24fed1810
rchavarria <txingo@gmail.com>
2015-02-23 17:00:55 +0100

    New test, when the GCD is the numerator or denominator because they are multiples

--- test/simplify-fractions/simplify_fractions_test.dart
+++ test/simplify-fractions/simplify_fractions_test.dart
@@ -9,8 +9,22 @@ void main() {
             expect(1, equals(gcd(1, 2)));
         });
 
+        test('numerator and denominator are multiples', () {
+            expect(2, equals(gcd(4, 2)));
+            expect(2, equals(gcd(2, 4)));
+        });
+
     });
 
 }
 
-int gcd(numerator, denominator) => 1;
+int gcd(numerator, denominator) {
+    while(denominator != 0) {
+        int t = denominator;
+        denominator = numerator % t;
+        numerator = t;
+    }
+
+    return numerator;
+}
+
