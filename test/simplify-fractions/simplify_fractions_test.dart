9c3475cd4f5b60f8fd12627b1803339eb8e749ea
rchavarria <txingo@gmail.com>
2015-02-25 09:03:40 +0100

    Inline some variables

--- test/simplify-fractions/simplify_fractions_test.dart
+++ test/simplify-fractions/simplify_fractions_test.dart
@@ -8,32 +8,28 @@ void main() {
         group('#simplify', () {
 
             test('does not mofidy already simplified members', () {
-                var fraction = new Fraction(2, 1);
-                var simplified = fraction.simplify();
+                var simplified = new Fraction(2, 1).simplify();
 
                 expect(2, equals(simplified.numerator));
                 expect(1, equals(simplified.denominator));
             });
 
             test('reduces members by a common factor', () {
-                var fraction = new Fraction(3, 6);
-                var simplified = fraction.simplify();
+                var simplified = new Fraction(3, 6).simplify();
 
                 expect(1, equals(simplified.numerator));
                 expect(2, equals(simplified.denominator));
             });
 
             test('does not reduce a fraction whose members are primes', () {
-                var fraction = new Fraction(13, 47);
-                var simplified = fraction.simplify();
+                var simplified = new Fraction(13, 47).simplify();
 
                 expect(13, equals(simplified.numerator));
                 expect(47, equals(simplified.denominator));
             });
 
             test('reduces a fraction by several common factors', () {
-                var fraction = new Fraction(1848, 990); // common factor 2 * 3 * 11 = 66
-                var simplified = fraction.simplify();
+                var simplified = new Fraction(1848, 990).simplify();
 
                 expect(28, equals(simplified.numerator));
                 expect(15, equals(simplified.denominator));
