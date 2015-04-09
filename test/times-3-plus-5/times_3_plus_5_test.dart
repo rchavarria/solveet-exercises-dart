421158169b48d808c93631068cd46de7e8cb6780
rchavarria <txingo@gmail.com>
2015-04-09 09:22:00 +0200

    Refactor : change the order of parameters for expect() method to get better tests error messages

--- test/times-3-plus-5/times_3_plus_5_test.dart
+++ test/times-3-plus-5/times_3_plus_5_test.dart
@@ -5,26 +5,26 @@ void main() {
     group('Times 3 - Plus 5', () {
 
         test('can not build a number by multiplying 1 by 3 only once', () {
-            expect(false, equals(buildableByMultiplyingBy3(4)));
-            expect(false, equals(buildableByMultiplyingBy3(6)));
-            expect(false, equals(buildableByMultiplyingBy3(10)));
+            expect(buildableByMultiplyingBy3(4), false);
+            expect(buildableByMultiplyingBy3(6), false);
+            expect(buildableByMultiplyingBy3(10), false);
         });
 
         test('can build a number by multiplying 1 by 3 only once', () {
-            expect(true, equals(buildableByMultiplyingBy3(3)));
+            expect(buildableByMultiplyingBy3(3), true);
         });
 
         test('can not build a number by multiplying 1 by 3 several times', () {
-            expect(false, equals(buildableByMultiplyingBy3(10)));
-            expect(false, equals(buildableByMultiplyingBy3(15)));
-            expect(false, equals(buildableByMultiplyingBy3(49)));
+            expect(buildableByMultiplyingBy3(10), false);
+            expect(buildableByMultiplyingBy3(15), false);
+            expect(buildableByMultiplyingBy3(49), false);
         });
 
         test('can build a number by multiplying 1 by 3 several times', () {
-            expect(true, equals(buildableByMultiplyingBy3(9)));
-            expect(true, equals(buildableByMultiplyingBy3(27)));
-            expect(true, equals(buildableByMultiplyingBy3(81)));
-            expect(true, equals(buildableByMultiplyingBy3(243)));
+            expect(buildableByMultiplyingBy3(9), true);
+            expect(buildableByMultiplyingBy3(27), true);
+            expect(buildableByMultiplyingBy3(81), true);
+            expect(buildableByMultiplyingBy3(243), true);
         });
 
     });
