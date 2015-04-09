8a26366b1c5be11e2b098d74bd6726feb8b31b8b
rchavarria <txingo@gmail.com>
2015-04-09 08:59:20 +0200

    Start working on a new Exercise : Times 3 - Plus 5

---
+++ test/times-3-plus-5/times_3_plus_5_test.dart
@@ -0,0 +1,14 @@
+import 'package:unittest/unittest.dart';
+
+void main() {
+
+    group('Times 3 - Plus 5', () {
+
+        test('tries to build a number only multiplying by 3', () {
+            expect(2, equals(1 + 1));
+        });
+
+    });
+
+}
+
