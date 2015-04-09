ac5322977a56b3c8b6ba01580b1e7895343ee714
rchavarria <txingo@gmail.com>
2015-04-09 09:11:33 +0200

    Add test to check that a number can be built by multiplying 1 by 3 only once

--- test/times-3-plus-5/times_3_plus_5_test.dart
+++ test/times-3-plus-5/times_3_plus_5_test.dart
@@ -4,17 +4,23 @@ void main() {
 
     group('Times 3 - Plus 5', () {
 
-        test('can not build a number by multiplying it by 3 only once', () {
+        test('can not build a number by multiplying 1 by 3 only once', () {
             expect(false, equals(buildableByMultiplyingBy3(4)));
             expect(false, equals(buildableByMultiplyingBy3(6)));
             expect(false, equals(buildableByMultiplyingBy3(10)));
+        });
+
+        test('can build a number by multiplying 1 by 3 only once', () {
+            expect(true, equals(buildableByMultiplyingBy3(3)));
         });
 
     });
 
 }
 
-boolean buildableByMultiplyingBy3(n) {
-    return false;
+boolean buildableByMultiplyingBy3(int n) {
+    int divided = (n / 3).toInt();
+    int modulus = n % 3;
+    return divided == 1 && modulus == 0;
 }
 
