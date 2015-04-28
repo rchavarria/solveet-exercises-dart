37a23daab30df317084de9b367eb47199e804f7d
rchavarria <txingo@gmail.com>
2015-04-28 16:01:44 +0200

    Refactor : replace if block with a call to IdentityBuilder

--- bin/times-3-plus-5/times_3_plus_5.dart
+++ bin/times-3-plus-5/times_3_plus_5.dart
@@ -1,13 +1,16 @@
+import './identity_builder.dart';
 import './times3_builder.dart';
 import './plus5_builder.dart';
 
 class Times3Plus5 {
     List steps;
+    IdentityBuilder identity;
     Times3Builder times3;
     Plus5Builder plus5;
 
     Times3Plus5() {
         steps = [];
+        identity = new IdentityBuilder();
         times3 = new Times3Builder(this);
         plus5 = new Plus5Builder(this);
     }
@@ -17,12 +20,10 @@ class Times3Plus5 {
             return false;
         }
 
-        if (n == 1) {
-            steps.add('1');
-            return true;
+        boolean buildable = identity.isBuildable(n);
+        if (!buildable) {
+            buildable = times3.isBuildable(n);
         }
-
-        boolean buildable = times3.isBuildable(n);
         if (!buildable) {
             buildable = plus5.isBuildable(n);
         }
