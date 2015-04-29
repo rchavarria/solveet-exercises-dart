c8fbf918c74d362d7ec66c90f353c2cf22a816b8
rchavarria <txingo@gmail.com>
2015-04-29 17:38:25 +0200

    Refactor : remove some innecessary class attributes

--- bin/times-3-plus-5/times_3_plus_5.dart
+++ bin/times-3-plus-5/times_3_plus_5.dart
@@ -4,15 +4,15 @@ import './plus5_builder.dart';
 
 class Times3Plus5 {
     List steps;
-    IdentityBuilder identity;
-    Times3Builder times3;
-    Plus5Builder plus5;
+    List builders;
 
     Times3Plus5() {
         steps = [];
-        identity = new IdentityBuilder();
-        times3 = new Times3Builder(this);
-        plus5 = new Plus5Builder(this);
+        builders = [
+            new IdentityBuilder(),
+            new Times3Builder(this),
+            new Plus5Builder(this)
+        ];
     }
 
     boolean isBuildable(int n) {
@@ -20,7 +20,6 @@ class Times3Plus5 {
             return false;
         }
 
-        var builders = [identity, times3, plus5];
         boolean buildable = false;
         builders.forEach((builder) {
             if (!buildable) {
