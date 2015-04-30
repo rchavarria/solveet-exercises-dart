2663194530421fa332f017d2545ac40bef6bb4df
rchavarria <txingo@gmail.com>
2015-04-30 08:53:45 +0200

    Refactor : reorganize conditions

--- bin/times-3-plus-5/times3_builder.dart
+++ bin/times-3-plus-5/times3_builder.dart
@@ -5,14 +5,19 @@ class Times3Builder {
     Times3Builder(this.delegate); 
     
     boolean isBuildable(int n) {
-        if (_isDivisible(n)) {
-            return this.delegate.isBuildable(n / 3);
+        if (n == 0) {
+            return false;
         }
-        return false;
+
+        if (_isNotDivisible(n)) {
+            return false;
+        }
+
+        return this.delegate.isBuildable(n / 3);
     }
 
-    boolean _isDivisible(n) {
-        return n != 0 && n % 3 == 0;
+    boolean _isNotDivisible(n) {
+        return n % 3 != 0;
     }
 
 }
